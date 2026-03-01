f0f1b02a36fcdb8a34a8c0f2e5d222c5f704acc0
krithiga <krithigashree804@gmail.com>
2026-03-01 20:47:36 +0530

    Update usecase4.java

--- usecase4.java
+++ usecase4.java
@@ -3,13 +3,13 @@ public class usecase4 {
 
         String[] banner = new String[7];   // declaration
 
-        banner[0] = "   ***      ***     *****     ****  ";
+        banner[0] = "   ***      ***     *****        ****  ";
         banner[1] = " **   **  **   **   **   **   **     ";
         banner[2] = "**     ****     **  **   **   **      ";
-        banner[3] = "**     ****     **  *****     *****   ";
-        banner[4] = "**     ****     **  **         **     ";
-        banner[5] = " **   **  **   **   **         **     ";
-        banner[6] = "   ***      ***     **         *****  ";
+        banner[3] = "**     ****     **  *****       *****   ";
+        banner[4] = "**     ****     **  **               **     ";
+        banner[5] = " **   **  **   **   **              **     ";
+        banner[6] = "   ***      ***     **        *****  ";
 
         for (String line : banner) {
             System.out.println(line);
