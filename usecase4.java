fe50a3c4d856dfb0ca34884bdf63a5b08fdc636c
krithiga <krithigashree804@gmail.com>
2026-03-01 20:46:56 +0530

    Updated usecase4.java

--- usecase4.java
+++ usecase4.java
@@ -1,14 +1,16 @@
 public class usecase4 {
     public static void main(String[] args) {
-        String[] banner = {
-            String.join("   ","   ***   ", "   ***   "," ***** ", "   **** "),
-            String.join("   "," **   ** ", " **   ** "," **   **", " **     "),
-            String.join("   ","**     **", "**     **"," **   **", "**      "),
-            String.join("   ","**     **", "**     **"," *****  ", "  ***** "),
-            String.join("   ","**     **", "**     **"," **     ", "      **"),
-            String.join("   "," **   ** ", " **   ** "," **     ", "**     **"),
-            String.join("   ","   ***   ", "   ***   "," **     ", "  ***** ")
-        };      
+
+        String[] banner = new String[7];   // declaration
+
+        banner[0] = "   ***      ***     *****     ****  ";
+        banner[1] = " **   **  **   **   **   **   **     ";
+        banner[2] = "**     ****     **  **   **   **      ";
+        banner[3] = "**     ****     **  *****     *****   ";
+        banner[4] = "**     ****     **  **         **     ";
+        banner[5] = " **   **  **   **   **         **     ";
+        banner[6] = "   ***      ***     **         *****  ";
+
         for (String line : banner) {
             System.out.println(line);
         }
